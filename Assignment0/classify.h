8638ee3352a6483ee1fe8c30df044e558271de2f
Shrey <2019CS10400@gmail.com>
2022-01-16 00:39:43 +0530

    Fixed a bug in header file; Modified makefile

--- Assignment0/classify.h
+++ Assignment0/classify.h
@@ -4,137 +4,165 @@
 #include <mutex>
 #include <iostream>
 
-#define MAXTHREADS	64 // Maximum number of threads supported
-#define BADRANGE	0
+#define MAXTHREADS 64 // Maximum number of threads supported
+#define BADRANGE 0
 
 struct Data;
 class Ranges;
 
 Data classify(Data &D, const Ranges &R, unsigned int numt);
 
-class alignas(32) Counter { // Aligned allocation per counter. Is that enough?
-			    // Keeps per-thread subcount.
+//Current alignment set to 32 bytes
+class alignas(32) Counter
+{  // Aligned allocation per counter. Is that enough?
+   // Keeps per-thread subcount.
 public:
-   Counter(unsigned int num=MAXTHREADS) {
+   Counter(unsigned int num = MAXTHREADS)
+   {
       _numcount = num;
       _counts = new unsigned int[num];
       assert(_counts != NULL);
       zero();
    }
 
-   void zero() { // Initialize
-      for(int i=0; i<_numcount; i++)
-	 _counts[i] = 0;
+   void zero()
+   { // Initialize
+      for (int i = 0; i < _numcount; i++)
+         _counts[i] = 0;
    }
 
-   void increase(unsigned int id) { // If each sub-counter belongs to a thread mutual exclusion is not needed
+   void increase(unsigned int id)
+   { // If each sub-counter belongs to a thread mutual exclusion is not needed
       assert(id < _numcount);
       _counts[id]++;
    }
 
-   void xincrease(unsigned int id) { // Safe increment
+   void xincrease(unsigned int id)
+   { // Safe increment
       assert(id < _numcount);
       const std::lock_guard<std::mutex> lock(cmutex);
       _counts[id]++;
    }
 
-   unsigned int get(unsigned int id) const { // return subcounter value for specific thread
+   unsigned int get(unsigned int id) const
+   { // return subcounter value for specific thread
       assert(id < _numcount);
       return _counts[id];
    }
 
-   void inspect() {
+   void inspect()
+   {
       std::cout << "Subcounts -- ";
-      for(int i=0; i<_numcount; i++)
-	 std::cout << i << ":" << _counts[i] << " ";
+      for (int i = 0; i < _numcount; i++)
+         std::cout << i << ":" << _counts[i] << " ";
       std::cout << "\n";
    }
 
 private:
    unsigned volatile int *_counts;
-   unsigned int _numcount; // Per-thread subcounts 
+   unsigned int _numcount; // Per-thread subcounts
    std::mutex cmutex;
 };
 
-struct Range { // Integer range
+struct Range
+{ // Integer range
 
-   Range(int a=1, int b=0) { // Constructor. Defaults to *bad* range
+   Range(int a = 1, int b = 0)
+   { // Constructor. Defaults to *bad* range
       lo = a;
       hi = b;
    }
 
-   bool within(int val) const { // Return if val is within this range
-       return(lo <= val && val <= hi);
+   bool within(int val) const
+   { // Return if val is within this range
+      return (lo <= val && val <= hi);
    }
 
-   bool strictlyin(int val) const { // Return if val is strictly inside this range
-       return(lo < val && val < hi);
+   bool strictlyin(int val) const
+   { // Return if val is strictly inside this range
+      return (lo < val && val < hi);
    }
 
    int lo;
-   int hi; 
+   int hi;
 };
 
-class Ranges {
+class Ranges
+{
 public:
-   Ranges() { // Initialize with a single unreal interval
+   Ranges()
+   { // Initialize with a single unreal interval
       _num = 1;
       _ranges = new Range(1, 0); // Started with this. Its not a real interval as nothing lies inside it.
    }
 
-   Ranges& operator+=(const Range range){ // Add one more interval to this list
-      if(newrange(range)) { // If it already exists, do not add
+   Ranges &operator+=(const Range range)
+   { // Add one more interval to this list
+      if (newrange(range))
+      { // If it already exists, do not add
          Range *oranges = _ranges;
-         _ranges = new Range[_num+1];
+         _ranges = new Range[_num + 1];
          assert(NULL != _ranges);
-         for(int r=0; r<_num; r++) { 
+         for (int r = 0; r < _num; r++)
+         {
             set(r, oranges[r].lo, oranges[r].hi); // copy old intervals
          }
          set(_num++, range.lo, range.hi); // Add the new interval at the end
       }
+      return *this;
    }
 
-   int range(int val, bool strict = false) const { // Tell the range in which val lies (strict => boundary match not ok)
-       if(strict) {
-          for(int r=0; r<_num; r++) // Look through all intervals
-             if(_ranges[r].strictlyin(val))
-                return r;
-       } else {
-          for(int r=0; r<_num; r++) // Look through all intervals
-             if(_ranges[r].within(val))
-                return r;
-       }
-       return BADRANGE; // Did not find any range
-   }
-
-   void inspect() {
-      for(int r=0; r<_num; r++) { 
-         std::cout << r << "," << &_ranges[r] << ": " << _ranges[r].lo << ", " << _ranges[r].hi << "\n"; 
+   int range(int val, bool strict = false) const
+   { // Tell the range in which val lies (strict => boundary match not ok)
+      if (strict)
+      {
+         for (int r = 0; r < _num; r++) // Look through all intervals
+            if (_ranges[r].strictlyin(val))
+               return r;
       }
+      else
+      {
+         for (int r = 0; r < _num; r++) // Look through all intervals
+            if (_ranges[r].within(val))
+               return r;
+      }
+      return BADRANGE; // Did not find any range
+   }
 
+   void inspect()
+   {
+      for (int r = 0; r < _num; r++)
+      {
+         std::cout << r << "," << &_ranges[r] << ": " << _ranges[r].lo << ", " << _ranges[r].hi << "\n";
+      }
    }
 
    int num() const { return _num; }
 
 private:
    Range *_ranges;
-   int   _num;
+   int _num;
 
-   void set(int i, int lo, int hi) { // set the extreme values of a specific interval
-      if(i < _num) {
-          _ranges[i].lo = lo;
-          _ranges[i].hi = hi;
+   void set(int i, int lo, int hi)
+   { // set the extreme values of a specific interval
+      if (i < _num)
+      {
+         _ranges[i].lo = lo;
+         _ranges[i].hi = hi;
       }
    }
 
-   bool newrange(const Range r) { // Is the range r already in my list, or is it a new one?
+   bool newrange(const Range r)
+   {                                                                           // Is the range r already in my list, or is it a new one?
       return (range(r.lo, true) == BADRANGE && range(r.hi, true) == BADRANGE); // Overlaps are not allowed.
    }
 };
 
-struct Data {
+struct Data
+{
 
-   struct Item {
+   struct Item
+   {
       int key;
       int value = -1;
    };
@@ -142,18 +170,21 @@ struct Data {
    unsigned int ndata = 0;
    Item *data = NULL;
 
-   Data(int n) { // n = Maximum number of items  storable
+   Data(int n)
+   { // n = Maximum number of items  storable
       ndata = n;
       data = new Item[n];
       assert(NULL != data);
    }
 
-   void reset() {
-      for(int i=0; i<ndata; i++)
+   void reset()
+   {
+      for (int i = 0; i < ndata; i++)
          data[i].value = -1;
    }
-   void inspect() {
-      for(int i=0; i<ndata; i++)
-         std::cout << i << ": " << data[i].key << " -- " << data[i].value <<"\n";
+   void inspect()
+   {
+      for (int i = 0; i < ndata; i++)
+         std::cout << i << ": " << data[i].key << " -- " << data[i].value << "\n";
    }
 };
--- Assignment0/main.cpp
+++ Assignment0/main.cpp
@@ -12,7 +12,7 @@ float timedwork(Data &D, const Ranges &R, unsigned int numt)
    auto end = std::chrono::high_resolution_clock::now();
    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
    // return(1e-6 * elapsed.count());
-   return(1e-6 * (std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)).count());
+   return (1e-6 * (std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)).count());
    // return mssince();
 }
 
@@ -20,44 +20,45 @@ void repeatrun(unsigned int reps, Data &D, const Ranges &R, unsigned int numt)
 { // Repeat the same experiment. Time each rep. D, R, and numt remain fixed.
    int it;
    float ms, tms = 0, minms = 1e7;
-   for(it=0; it<reps; it++) {
+   for (it = 0; it < reps; it++)
+   {
       tms += ms = timedwork(D, R, numt); // This is the real work
       std::cout << ms << " ms\n";
-      usleep(1000); // Lose control for 1 ms 
-      if(ms < minms) minms = ms;
+      usleep(1000); // Lose control for 1 ms
+      if (ms < minms)
+         minms = ms;
       D.reset();
    }
-   std::cout << reps << " iterations of " << D.ndata << " items in " 
+   std::cout << reps << " iterations of " << D.ndata << " items in "
              << R.num() << " ranges with " << numt << " threads: Fastest took "
-             << minms << " ms, Average was " << tms/reps << " ms\n";
+             << minms << " ms, Average was " << tms / reps << " ms\n";
 }
 
 Ranges readRanges(const char *filename)
 {
-    Ranges R;
-    std::fstream fs(filename, std::fstream::in);
+   Ranges R;
+   std::fstream fs(filename, std::fstream::in);
 
-    int lo, hi;
-    while (fs >> lo >> hi)
-        R += Range(lo, hi);
-    fs.close();
-
-    return R;
+   int lo, hi;
+   while (fs >> lo >> hi)
+      R += Range(lo, hi);
+   fs.close();
+   return R;
 }
 
 Data readData(const char *filename, unsigned int nItems)
 {
-    assert(nItems > 0);
-    Data D(nItems);
-    std::fstream fs(filename, std::fstream::in);
-    int d = 0;
-    while (fs >> D.data[d++].key)
-       if(d == nItems)
-          break;
-    if(d < nItems)
-       D.ndata = d-1;
-    fs.close();
-    return D;
+   assert(nItems > 0);
+   Data D(nItems);
+   std::fstream fs(filename, std::fstream::in);
+   int d = 0;
+   while (fs >> D.data[d++].key)
+      if (d == nItems)
+         break;
+   if (d < nItems)
+      D.ndata = d - 1;
+   fs.close();
+   return D;
 }
 
 int main(int argc, char *argv[])
@@ -66,8 +67,8 @@ int main(int argc, char *argv[])
    Ranges R = readRanges(argv[1]);
    Data D = readData(argv[2], atoi(argv[3]));
 
-   int reps = 6; // 6 reps by default
-   if(argc == 6) // If there is a reps arg, use it
+   int reps = 6;  // 6 reps by default
+   if (argc == 6) // If there is a reps arg, use it
       reps = atoi(argv[5]);
 
    repeatrun(reps, D, R, atoi(argv[4])); // repeat reps times
